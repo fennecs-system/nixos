e11d08ef6c4f4e33efc6294f69922ef0baea97a7
Fennecs <meswaddle@gmail.com>
2025-11-05 17:47:20 +1100

    feat: state version

--- home/users/fennecs/default.nix
+++ home/users/fennecs/default.nix
@@ -1,5 +1,62 @@
-{}: 
+{ config, pkgs, version, ... }:
 
-{ 
-  import ./home.nix;
+{
+
+  home.stateVersion = version;
+  
+  home.packages = with pkgs; [
+    r2modman
+    element-desktop
+    firefox
+    telegram-desktop
+    ghc
+    vim
+    mpv
+    perf-tools
+    perf
+    (symlinkJoin {
+      name = "vscode-with-extensions";
+      paths = [
+        (vscode-with-extensions.override {
+          vscodeExtensions = with vscode-extensions; [
+            jnoortheen.nix-ide
+            ms-python.python
+            gleam.gleam
+            ms-vscode-remote.remote-containers
+            ms-vscode-remote.vscode-remote-extensionpack
+            ms-vscode.hexeditor
+            justusadam.language-haskell
+            haskell.haskell
+            github.copilot
+          ];
+        })
+      ];
+      buildInputs = [ makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/code --add-flags "--disable-gpu"
+      '';
+    })
+    unzip
+    prismlauncher
+    jdk
+    gleam
+    nixfmt
+    erlang
+    easyeffects
+    git
+    haskellPackages.pointfree
+    vesktop
+    krita
+  ];
+
+  # You can also set your shell in Home Manager
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      # Commands to run in interactive sessions can go here
+    '';
+    shellInit = ''
+      fish_add_path $HOME/.cargo/bin
+    '';
+  };
 }
