14cac43b8e7240a1c1b1d90b10f99bbd5bd2cb71
Fennecs <meswaddle@gmail.com>
2025-11-07 17:48:15 +1100

    chore: sway

---
+++ home/users/fennecs/sway/sway.nix
@@ -0,0 +1,72 @@
+{ pkgs, ...}:
+let 
+  colors = import ./colors.nix;
+  accent_color = colors.accent_color;
+  bg_color = colors.bg_color;
+  text_color = colors.text_color;
+  fg_color = colors.fg_color;
+  urgent_color = colors.urgent_color;
+  placeholder_color = colors.placeholder_color;
+in
+{
+    wayland.windowManager.sway = {
+    enable = true;
+    systemd.enable = true;
+    wrapperFeatures.gtk = true;
+
+    # use meta/windows key
+    config = rec {
+      modifier = "Mod4";
+
+      gaps = {
+        smartGaps = true;
+        outer = 8;
+        inner = 4;
+      };
+
+      window = {
+        border = 1;
+        hideEdgeBorders = "smart";
+      };
+
+      colors = {
+        background = bg_color; 
+        focused = {
+          border = accent_color;
+          background = bg_color ;
+          text = accent_color;
+          indicator = accent_color;
+          childBorder = accent_color;        
+        };
+        focusedInactive = {
+          border = fg_color;
+          background = bg_color;
+          text = text_color;
+          indicator = fg_color;
+          childBorder = fg_color;
+        };
+        unfocused = {
+          border = fg_color;
+          background = bg_color;
+          text = text_color;
+          indicator = fg_color;
+          childBorder = fg_color;
+        };
+        urgent = {                  
+          border = urgent_color;
+          background = bg_color;
+          text = text_color;
+          indicator = urgent_color;
+          childBorder = urgent_color;
+        };
+        placeholder = {
+          border = placeholder_color;
+          background = bg_color;
+          text = text_color;
+          indicator = placeholder_color;
+          childBorder = placeholder_color;
+        };
+      };
+    };
+  };
+}
---
+++ home/users/fennecs/sway/waybar.nix
@@ -0,0 +1,12 @@
+{config, pkgs, ...}:
+{
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+      };
+    };
+  };
+}
