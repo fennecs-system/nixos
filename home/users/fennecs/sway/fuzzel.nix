94e95f2ac68425e1d3c9e95081abdfd4a34a7dce
Fennecs <meswaddle@gmail.com>
2025-11-07 20:42:50 +1100

    chore: fuzzel

--- home/users/fennecs/sway/fuzzel.nix
+++ home/users/fennecs/sway/fuzzel.nix
@@ -1,12 +1,22 @@
 { config, pkgs, lib, ...}: 
 let 
   modifier = config.wayland.windowManager.sway.config.modifier;
+  colors = import ./colors.nix;
+  bg_color = colors.bg_color;
+  text_color = colors.text_color;
+  selection_color = colors.urgent_color;
 in
 {
   programs.fuzzel.enable = true; 
 
   programs.fuzzel.settings.config = {
     dpi-aware = true;
+    font = "size=36";
+    colors = {
+      background = bg_color;
+      selection = selection_color;
+      text = text_color;
+    }
   };
 
   wayland.windowManager.sway.config = {
