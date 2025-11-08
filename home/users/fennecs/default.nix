e0a4049361841259c50e06f7cee92730ff4119b1
Fennecs <meswaddle@gmail.com>
2025-11-08 21:33:53 +1100

    chore: rounded

--- home/users/fennecs/default.nix
+++ home/users/fennecs/default.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, version, ... }:
+{
+  config,
+  pkgs,
+  version,
+  ...
+}:
 
 {
 
@@ -23,16 +28,53 @@
       flavor = "mocha";
     };
 
+    vesktop = {
+      enable = true;
+      flavor = "mocha";
+      accent = "pink";
+    };
+
     firefox = {
-      enable = true; 
+      enable = true;
       flavor = "mocha";
       accent = "pink";
       force = true;
     };
   };
 
+  programs.firefox.enable = true;
 
+  programs.vesktop.enable = true;
 
+  programs.vscode = {
+    enable = true;
+    package = pkgs.symlinkJoin {
+      name = "vscode-with-extensions";
+      pname = "vscode";
+      version = pkgs.vscode.version;
+      paths = [
+        (pkgs.vscode-with-extensions.override {
+          vscodeExtensions = with pkgs.vscode-extensions; [
+            jnoortheen.nix-ide
+            ms-python.python
+            gleam.gleam
+            ms-vscode-remote.remote-containers
+            ms-vscode-remote.vscode-remote-extensionpack
+            ms-vscode.hexeditor
+            justusadam.language-haskell
+            haskell.haskell
+            github.copilot
+            catppuccin.catppuccin-vsc
+            catppuccin.catppuccin-vsc-icons
+          ];
+        })
+      ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/code --add-flags "--disable-gpu"
+      '';
+    };
+  };
 
   gtk = {
     enable = true;
@@ -49,7 +91,7 @@
   };
 
   home.stateVersion = version;
-  fonts.fontconfig.enable = true; 
+  fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
     maple-mono.variable
     maple-mono.truetype
@@ -57,35 +99,12 @@
     maple-mono.NF-CN-unhinted
     r2modman
     element-desktop
-    firefox
     telegram-desktop
     ghc
     vim
     mpv
     perf-tools
     perf
-    (symlinkJoin {
-      name = "vscode-with-extensions";
-      paths = [
-        (vscode-with-extensions.override {
-          vscodeExtensions = with vscode-extensions; [
-            jnoortheen.nix-ide
-            ms-python.python
-            gleam.gleam
-            ms-vscode-remote.remote-containers
-            ms-vscode-remote.vscode-remote-extensionpack
-            ms-vscode.hexeditor
-            justusadam.language-haskell
-            haskell.haskell
-            github.copilot
-          ];
-        })
-      ];
-      buildInputs = [ makeWrapper ];
-      postBuild = ''
-        wrapProgram $out/bin/code --add-flags "--disable-gpu"
-      '';
-    })
     unzip
     prismlauncher
     jdk
@@ -95,7 +114,6 @@
     easyeffects
     git
     haskellPackages.pointfree
-    vesktop
     krita
   ];
 
