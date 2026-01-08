417f7d04667f210ee8c5e1c6f0b1415e3d47b25d
Fennecs <meswaddle@gmail.com>
2026-01-08 19:14:42 +1100

    chore: bump deps

--- flake.nix
+++ flake.nix
@@ -11,10 +11,25 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
+
+
   };
   outputs =
     inputs@{
       self,
+      lix-module, 
+      lix,
       nixpkgs,
       home-manager,
       catppuccin,
@@ -37,7 +52,10 @@
               inherit inputs;
               version = hostSystem.version;
             };
-            modules = [ ./hosts/${hostname} ];
+            modules = [ 
+              ./hosts/${hostname} 
+              lix-module.nixosModules.default  
+            ];
           };
         };
 
